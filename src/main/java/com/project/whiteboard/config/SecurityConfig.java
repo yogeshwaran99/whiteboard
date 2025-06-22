a65daacaf5b81f51594d82868ea05bc5976ce98f
yogeshwaran99 <itsyogeshwaran@gmail.com>
2025-06-23 01:23:30 +0530

    Fix deprecation

--- src/main/java/com/project/whiteboard/config/SecurityConfig.java
+++ src/main/java/com/project/whiteboard/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.project.whiteboard.config;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,48 +12,40 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-	@Autowired	
+	@Autowired
 	private UserDetailsService userDetailsService;
-	
+
 	@Bean
 	public AuthenticationProvider authProvider() {
-		DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
-		provider.setUserDetailsService(userDetailsService);
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
 		provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
 		return provider;
 	}
-		
-	
+
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-	    http.csrf(csrf -> csrf.disable())
-	        .authorizeHttpRequests(request -> request
-	            .requestMatchers("/", "/index.html", "/signup").permitAll()
-	            .anyRequest().authenticated()
-	        )
-	        .formLogin(form -> form
-	            .loginPage("/index.html") 
-	            .loginProcessingUrl("/login")
-	            .defaultSuccessUrl("/board.html", true)
-	            .permitAll()
-	        )
-	        .logout(logout -> logout
-	            .logoutUrl("/logout")
-	            .logoutSuccessUrl("/index.html")
-	            .permitAll()
-	        )
-	        .authenticationProvider(authProvider()) 
-	        .sessionManagement(session -> session
-	            .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-	        );
+		http.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(request -> request
+						.requestMatchers("/", "/index.html", "/signup").permitAll()
+						.anyRequest().authenticated())
+				.formLogin(form -> form
+						.loginPage("/index.html")
+						.loginProcessingUrl("/login")
+						.defaultSuccessUrl("/board.html", true)
+						.permitAll())
+				.logout(logout -> logout
+						.logoutUrl("/logout")
+						.logoutSuccessUrl("/index.html")
+						.permitAll())
+				.authenticationProvider(authProvider())
+				.sessionManagement(session -> session
+						.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
 
-	    return http.build();
+		return http.build();
 	}
-	
-	 
+
 }
